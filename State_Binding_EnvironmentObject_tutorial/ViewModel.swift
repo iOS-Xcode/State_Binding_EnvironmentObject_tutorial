36abbb9ee62485e88fb30f64cd7fcf0e3febaa88
Seokhyun Kim <973630+iOS-Xcode@users.noreply.github.com>
2023-01-08 20:10:08 +0900

    Update ViewModel.swift

--- State_Binding_EnvironmentObject_tutorial/ViewModel.swift
+++ State_Binding_EnvironmentObject_tutorial/ViewModel.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 class ViewModel: ObservableObject {
-    //시작하자마자 @Published 를 통해 값이 보내 진다.
+    //시작하자마자 @Published 를 통해 값이 ContentView 쪽으로 값을 전송한다.
     @Published var appTitle : String = "Seokhyun's Life"
 }
